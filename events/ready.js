a1c65284e9bc933c4b3e98ecc857dad9d0dc348c
The Fat Bastid <tfb@sof.ws>
2025-10-22 17:41:31 -0400

    Added member count to bot activity
    and adjusted the readme

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,17 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		
+		// Function to update the bot’s presence text
+		function updatePresence() {
+			const totalMembers = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+			client.user.setActivity(`${totalMembers} members`, { type: ActivityType.Watching });
+		}
+		
+		// Set initial presence
+		updatePresence();
+
+		// Update every 5 minutes
+		setInterval(updatePresence, 5 * 60 * 1000);
 	},
 };
