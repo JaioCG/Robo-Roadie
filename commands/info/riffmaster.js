c89bdc9d8e8f8c22bebcd936fc0d545b6c70cbaf
The Fat Bastid <tfb@sof.ws>
2025-10-22 17:21:19 -0400

    format adjustment

--- commands/info/riffmaster.js
+++ commands/info/riffmaster.js
@@ -6,6 +6,6 @@ module.exports = {
 		.setDescription('🎸 How to fix the riffmaster crash'),
 
 	async execute(interaction) {
-		await interaction.reply('# 🎸 How to fix the riffmaster crash\nIn Windows, if your riffmaster is crashing YARG or your entire computer try the following fix:\n\nOpen "Windows Settings" than "Add/Remove Programs" and look for a package named "Microsoft GameInput". Check if there are 2 versions installed. If so, uninstall the one that was installed most recently (ask if unsure). That is the version causing the issue.\nAfter uninstalling, plug the dongle back and it should work fine.');
+		await interaction.reply('# 🎸 How to fix the riffmaster crash\nIn Windows, if either your Riffmaster or Wii guitar via WiitarThing is crashing YARG or your entire computer, try the following fix:\n\n1. Open the Settings app -> Apps -> Installed Apps (or search for Add and Remove Programs)\n2. look for a package named "Microsoft GameInput". Check if there are 2 versions installed.\n3. If so, uninstall the one that was installed most recently (ask if unsure). That is the version causing the issue.\n4. After uninstalling, unplug/replug the dongle and restart YARG.');
 	},
 };
